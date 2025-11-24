049cb32ec794728cf5d125db386b9a39c55301c5
lekzytradepro <lekzy0002@gmail.com>
2025-11-24 06:47:24 +0100

    Update subscription_model.py

--- storage/subscription_model.py
+++ storage/subscription_model.py
@@ -1,1 +1,18 @@
+from .database import db
 
+class SubscriptionModel:
+    @staticmethod
+    def set_subscription(user_id, plan, expiry_date):
+        db.query("""
+        INSERT OR REPLACE INTO subscriptions (user_id, plan, expiry_date)
+        VALUES (?, ?, ?)
+        """, (user_id, plan, expiry_date))
+
+    @staticmethod
+    def get_subscription(user_id):
+        result = db.query("SELECT * FROM subscriptions WHERE user_id = ?", (user_id,))
+        return result.fetchone()
+
+    @staticmethod
+    def remove_subscription(user_id):
+        db.query("DELETE FROM subscriptions WHERE user_id = ?", (user_id,))
