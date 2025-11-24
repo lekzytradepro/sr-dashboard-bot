4a1e0c9e2fc849764be8f97470296b9d6beefc66
lekzytradepro <lekzy0002@gmail.com>
2025-11-24 06:25:11 +0100

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,1 +1,18 @@
+"""
+Engine package initializer.
+This exposes the main engine components for easy importing.
+"""
 
+from .market_analyzer import MarketAnalyzer
+from .pair_detector import PairDetector
+from .signal_engine import SignalEngine
+from .signal_processor import SignalProcessor
+from .strategy_engine import StrategyEngine
+
+__all__ = [
+    "MarketAnalyzer",
+    "PairDetector",
+    "SignalEngine",
+    "SignalProcessor",
+    "StrategyEngine"
+]
