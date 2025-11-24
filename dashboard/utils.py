d28f804dc950221a57c00371efcdcb8ff2470313
lekzytradepro <lekzy0002@gmail.com>
2025-11-24 06:23:04 +0100

    Update utils.py

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,1 +1,8 @@
+from dashboard.models import ADMIN_USERS
 
+def is_admin(username: str):
+    return username in ADMIN_USERS
+
+def register_admin(username: str, password: str):
+    ADMIN_USERS.add(username)
+    return True
